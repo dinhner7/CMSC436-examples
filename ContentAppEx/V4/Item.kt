5e7d8a4fc3be883d4bebb830641ecfe0afbbacfb
Dinh Huynh <157769040+dinhner7@users.noreply.github.com>
2024-11-13 21:15:35 -0500

    Add files via upload

---
+++ ContentAppEx/V4/Item.kt
@@ -0,0 +1,26 @@
+package com.example.xmlcontentappv4
+
+class Item {
+    private var title : String = ""
+    private var link : String = ""
+
+    fun getTitle( ) : String {
+        return title
+    }
+
+    fun getLink( ) : String {
+        return link
+    }
+
+    fun setLink( link : String ) {
+        this.link = link
+    }
+
+    fun setTitle( title : String ) {
+        this.title = title
+    }
+
+    override fun toString( ) : String {
+        return "title: " + title + "; link: " + link
+    }
+}
